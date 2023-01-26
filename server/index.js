f6e9056fce465bcbaf15702bf3676cdeb0199701
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-01-26 10:29:49 +0530

    Basic Setup done

---
+++ server/index.js
@@ -0,0 +1,48 @@
+// Relevant Libraries
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+import multer from "multer"
+import helmet from "helmet"
+import morgan from "morgan"
+import path from "path"
+import { fileURLToPath } from "url"
+
+// Configs
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
+app.use(morgan("common"))
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+app.use("/assets", express.static(path.join(__dirname, "public/assets")))
+
+// File Storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+// Database setup
+const PORT = process.env.PORT || 6001;
+mongoose.set('strictQuery', true);
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("MongoDB connected");
+  }
+);
+app.listen(PORT, ()=>console.log("Listening!"));
