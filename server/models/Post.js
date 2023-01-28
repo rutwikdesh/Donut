e8dcd12655c8817d330145b9416c951046de6a2d
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-01-28 21:59:29 +0530

    added posts controller

---
+++ server/models/Post.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const PostSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstname: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", PostSchema);
+
+export default Post;
