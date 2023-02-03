aba606da1599872b162e7174ec70f92c873a0fb1
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-02-03 12:15:30 +0530

    added base login template

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,22 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1.5rem 10%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="36px" color="primary">
+          Donut
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Donut
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
