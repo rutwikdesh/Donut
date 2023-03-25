b21662a09f4e9f78a9041cda63b1bfd8e3da80cf
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-03-25 17:23:50 +0530

    removed friend option for self account

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -20,6 +20,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   const medium = palette.neutral.medium;
 
   const isFriend = friends.find((friend) => friend._id === friendId);
+  const loggedInUserId = useSelector((state) => state.user._id);
 
   const patchFriend = async () => {
     const response = await fetch(
@@ -64,16 +65,17 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
           </Typography>
         </Box>
       </FlexBetween>
-      <IconButton
-        onClick={() => patchFriend()}
-        sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
-      >
-        {isFriend ? (
-          <PersonRemoveOutlined sx={{ color: primaryDark }} />
-        ) : (
-          <PersonAddOutlined sx={{ color: primaryDark }} />
-        )}
-      </IconButton>
+      {loggedInUserId != friendId && 
+        <IconButton
+          onClick={() => patchFriend()}
+          sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+        >
+          {isFriend ? (
+            <PersonRemoveOutlined sx={{ color: primaryDark }} />
+          ) : (
+            <PersonAddOutlined sx={{ color: primaryDark }} />
+          )}
+        </IconButton>}
     </FlexBetween>
   );
 };
